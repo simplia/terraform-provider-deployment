58888312ae75d817f558fa253b85a064d4335054
Petr Soukup <soukup@simplia.cz>
2021-02-15 17:33:17 +0100

    rename resource

--- provider.go
+++ provider.go
@@ -16,7 +16,7 @@ func Provider() *schema.Provider {
 		},
 
 		DataSourcesMap: map[string]*schema.Resource{
-			"current_deployment_ecs": dataSourceSimpliaEcsCurrentDeployment(),
+			"deployment_ecs": dataSourceSimpliaEcsCurrentDeployment(),
 		},
 	}
 
